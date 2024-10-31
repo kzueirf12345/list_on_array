f16b7e9e2f1bfd25f816ce4058f8f8c003c3e2da
Vladimir <jaba0192837465@gmail.com>
2024-10-31 16:17:13 +0300

    verification

--- src/verification/verification.c
+++ src/verification/verification.c
@@ -1,3 +1,11 @@
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "verification.h"
 #include "logger/liblogger.h"
 
@@ -8,6 +16,25 @@ const char* fist_strerror(const enum FistError error)
     {
         CASE_ENUM_TO_STRING_(FIST_ERROR_SUCCESS);
         CASE_ENUM_TO_STRING_(FIST_ERROR_STANDARD_ERRNO);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_FIST_IS_NULL);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_FIST_IS_NVALID);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_DATA_IS_NULL);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_DATA_IS_NVALID);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_NEXT_IS_NULL);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_NEXT_IS_NVALID);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_PREV_IS_NULL);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_PREV_IS_NVALID);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_ELEM_SIZE_IS_ZERO);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_NEXT_NVALID);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_NEXT_ELEM_OVERFLOW);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_PREV_NVALID);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_PREV_ELEM_OVERFLOW);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_FREE_ELEM_OVERFLOW);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_FREE_INTERSECT_NEXT);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_FREE_NCOMPLETE);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_SIZE_BIGGER_CAPACITY);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_CAPACITY_IS_ZERO);
+        CASE_ENUM_TO_STRING_(FIST_ERROR_UNKNOWN);
     default:
         return "UNKNOWN_FIST_ERROR";
     }
@@ -15,7 +42,166 @@ const char* fist_strerror(const enum FistError error)
 }
 #undef CASE_ENUM_TO_STRING_
 
-// enum FistError fist_verify_NOT_USE(const fist_t* const fist, const place_in_code_t call_place)
-// {
-//     lassert()
-// }
+
+#ifndef NDEBUG
+
+enum PtrState
+{
+    PTR_STATES_VALID   = 0,
+    PTR_STATES_NULL    = 1,
+    PTR_STATES_NVALID  = 2,
+    PTR_STATES_ERROR   = 3
+};
+static_assert(PTR_STATES_VALID == 0);
+
+static enum PtrState is_valid_ptr_(const void* const ptr);
+
+enum FistError fist_verify_NOT_USE(const fist_t* const fist)
+{
+
+    switch (is_valid_ptr_(fist))
+    {
+        case PTR_STATES_VALID:      break;
+        case PTR_STATES_NULL:       return FIST_ERROR_FIST_IS_NULL;
+        case PTR_STATES_NVALID:     return FIST_ERROR_FIST_IS_NVALID;
+        case PTR_STATES_ERROR:      return FIST_ERROR_STANDARD_ERRNO;
+        
+        default:
+            fprintf(stderr, "Unknown PtrState enum fist, it's soooo bad\n");
+            return FIST_ERROR_UNKNOWN;
+    }
+
+    switch (is_valid_ptr_(fist->data))
+    {
+        case PTR_STATES_VALID:      break;
+        case PTR_STATES_NULL:       return FIST_ERROR_DATA_IS_NULL;
+        case PTR_STATES_NVALID:     return FIST_ERROR_DATA_IS_NVALID;
+        case PTR_STATES_ERROR:      return FIST_ERROR_STANDARD_ERRNO;
+        
+        default:
+            fprintf(stderr, "Unknown PtrState enum fist->data, it's soooo bad\n");
+            return FIST_ERROR_UNKNOWN;
+    }
+
+    switch (is_valid_ptr_(fist->next))
+    {
+        case PTR_STATES_VALID:      break;
+        case PTR_STATES_NULL:       return FIST_ERROR_NEXT_IS_NULL;
+        case PTR_STATES_NVALID:     return FIST_ERROR_NEXT_IS_NVALID;
+        case PTR_STATES_ERROR:      return FIST_ERROR_STANDARD_ERRNO;
+        
+        default:
+            fprintf(stderr, "Unknown PtrState enum fist->next, it's soooo bad\n");
+            return FIST_ERROR_UNKNOWN;
+    }
+
+    switch (is_valid_ptr_(fist->prev))
+    {
+        case PTR_STATES_VALID:      break;
+        case PTR_STATES_NULL:       return FIST_ERROR_PREV_IS_NULL;
+        case PTR_STATES_NVALID:     return FIST_ERROR_PREV_IS_NVALID;
+        case PTR_STATES_ERROR:      return FIST_ERROR_STANDARD_ERRNO;
+        
+        default:
+            fprintf(stderr, "Unknown PtrState enum fist->prev, it's soooo bad\n");
+            return FIST_ERROR_UNKNOWN;
+    }
+
+
+    if (!fist->capacity)                return FIST_ERROR_CAPACITY_IS_ZERO;
+    if (!fist->elem_size)               return FIST_ERROR_ELEM_SIZE_IS_ZERO;
+    if ( fist->capacity < fist->size)   return FIST_ERROR_SIZE_BIGGER_CAPACITY;
+
+
+    // for (size_t max_deep = 1; max_deep < fist->size; ++max_deep)
+    // {
+    //     size_t cur_ind = 0;
+    //     size_t deep = 0;
+
+    //     for (; deep < max_deep && cur_ind != fist->prev[0]; 
+    //          ++deep,              cur_ind  = fist->next[cur_ind])
+    //     {
+    //         if (cur_ind > fist->capacity) return FIST_ERROR_NEXT_ELEM_OVERFLOW;
+    //     }
+    //     if ((max_deep == fist->size - 1) ^ (cur_ind == fist->prev[0]) ^ (fist->next[cur_ind] == 0)) 
+    //         return FIST_ERROR_NEXT_NVALID;
+
+        
+    //     for (; deep > 0        && cur_ind != fist->next[0];
+    //          --deep,              cur_ind  = fist->prev[cur_ind])
+    //     {
+    //         if (cur_ind > fist->capacity) return FIST_ERROR_PREV_ELEM_OVERFLOW;
+    //     }
+    //     if ((max_deep == fist->size - 1) ^ (cur_ind == fist->next[0]) ^ (fist->prev[cur_ind] == 0)) 
+    //         return FIST_ERROR_PREV_NVALID;
+    // }
+
+
+    size_t free_size = 0;
+    for (size_t free_ind = fist->free; free_ind != 0; free_ind = fist->next[free_ind])
+    {
+        if (free_ind > fist->capacity) return FIST_ERROR_FREE_ELEM_OVERFLOW;
+
+        for (size_t cur_ind = 0; cur_ind != fist->prev[0]; cur_ind = fist->next[cur_ind])
+        {
+            if (free_ind == cur_ind) return FIST_ERROR_FREE_INTERSECT_NEXT;
+        }
+        ++free_size;
+    }
+    
+    if (free_size + fist->size != fist->capacity) return FIST_ERROR_FREE_NCOMPLETE;
+
+    return FIST_ERROR_SUCCESS;
+}
+
+static enum PtrState is_valid_ptr_(const void* const ptr)
+{
+    if (errno)
+    {
+        perror("Errno enter in valid ptr check with error");
+        return PTR_STATES_ERROR;
+    }
+
+    if (ptr == NULL)
+    {
+        return PTR_STATES_NULL;
+    }
+
+    char filename[] = "/tmp/chupapi_munyanya.XXXXXX";
+    const int fd = mkstemp(filename);
+
+    if (fd == -1) 
+    {
+        perror("Can't mkstemp file");
+        return PTR_STATES_ERROR;
+    }
+
+    const ssize_t write_result = write(fd, ptr, 1);
+
+    if (remove(filename))
+    {
+        perror("Can't remove temp file");
+        return PTR_STATES_ERROR;
+    }
+
+    if (close(fd))
+    {
+        perror("Can't close temp file");
+        return PTR_STATES_ERROR;
+    }
+
+    if (write_result == 1) 
+    {
+        return PTR_STATES_VALID;
+    } 
+    else if (errno == EFAULT) 
+    {
+        errno = 0;
+        return PTR_STATES_NVALID;
+    }
+    
+    perror("Unpredictable errno state, after write into temp file");
+    return PTR_STATES_ERROR;
+}
+
+#endif /*NDEBUG*/
--- src/verification/verification.h
+++ src/verification/verification.h
@@ -1,14 +1,37 @@
 #ifndef LIST_ON_ARRAY_SRC_VERIFICATION_VERIFICATION_H
 #define LIST_ON_ARRAY_SRC_VERIFICATION_VERIFICATION_H
 
-#include "../fist/fist.h"
+#include "assert.h"
+
+#include "../fist/fist_structs.h"
 #include "logger/liblogger.h"
 
 enum FistError
 {
-    FIST_ERROR_SUCCESS          = 0,
-    FIST_ERROR_STANDARD_ERRNO   = 1
+    FIST_ERROR_SUCCESS              = 0,
+    FIST_ERROR_STANDARD_ERRNO       = 1,
+    FIST_ERROR_FIST_IS_NULL         = 2,
+    FIST_ERROR_FIST_IS_NVALID       = 3,
+    FIST_ERROR_DATA_IS_NULL         = 4,
+    FIST_ERROR_DATA_IS_NVALID       = 5,
+    FIST_ERROR_NEXT_IS_NULL         = 6,
+    FIST_ERROR_NEXT_IS_NVALID       = 7,
+    FIST_ERROR_PREV_IS_NULL         = 8,
+    FIST_ERROR_PREV_IS_NVALID       = 9,
+    FIST_ERROR_ELEM_SIZE_IS_ZERO    = 10,
+    FIST_ERROR_NEXT_NVALID          = 11,
+    FIST_ERROR_PREV_NVALID          = 12,
+    FIST_ERROR_NEXT_ELEM_OVERFLOW   = 13,
+    FIST_ERROR_PREV_ELEM_OVERFLOW   = 14,
+    FIST_ERROR_FREE_ELEM_OVERFLOW   = 15,
+    FIST_ERROR_FREE_INTERSECT_NEXT  = 16,
+    FIST_ERROR_FREE_NCOMPLETE       = 17,
+    FIST_ERROR_SIZE_BIGGER_CAPACITY = 18,
+    FIST_ERROR_CAPACITY_IS_ZERO     = 19,
+    FIST_ERROR_POP_ARG_NVALID       = 20,
+    FIST_ERROR_UNKNOWN              = 30
 };
+static_assert(FIST_ERROR_SUCCESS == 0);
 
 const char* fist_strerror(const enum FistError error);
 
@@ -25,7 +48,23 @@ const char* fist_strerror(const enum FistError error);
     } while(0)
 
 
-// enum FistError fist_verify_NOT_USE(const fist_t* const fist, const place_in_code_t call_place);
+#ifndef NDEBUG
 
+enum FistError fist_verify_NOT_USE(const fist_t* const fist);
+
+#define FIST_VERIFY(fist, elem_to_str)                                                              \
+        do {                                                                                        \
+            const enum FistError error = fist_verify_NOT_USE(fist);                                 \
+            if (error)                                                                              \
+            {                                                                                       \
+                lassert(false, "Fist error: %s", fist_strerror(error));                             \
+            }                                                                                       \
+        } while(0)
+
+#else /*NDEBUG*/
+
+#define FIST_VERIFY(fist, elem_to_str) do {} while(0)
+
+#endif /*NDEBUG*/
 
 #endif /*LIST_ON_ARRAY_SRC_VERFICATION_VERIFICATION_H*/
